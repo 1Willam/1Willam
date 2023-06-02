1f5107768624a59321c166f643f252987cc69024
1Willam <72182022+1Willam@users.noreply.github.com>
2023-06-01 20:10:19 -0500

    Update Ejercicios_lind_E_2_51.py

--- Ejercicios_lind_E_2_51.py
+++ Ejercicios_lind_E_2_51.py
@@ -29,14 +29,61 @@ k = np.round(np.emath.logn(2, len(df_goodyear["Price"])), 0)
 
 i = np.round((max(df_goodyear["Price"])-min(df_goodyear["Price"]))/k, 1)
 
+# Limites de clase
 
-## Histograma ##
+lim_s = pd.interval_range(start=125, periods=6, freq=35, closed='left')
+lim_i = pd.interval_range(start=335, periods=1, freq=35, closed='both')
+# pd.interval_range() retorna un IntervalIndex con los intervalos que queremos
+lim = lim_s.append(lim_i)
+# .append() agrega otros valores a una lista
+df_frecuency = pd.DataFrame({
+  'Price':lim,
+  'frecuency':np.nan,
+  'relative frecuency':np.nan,
+  'cumulative frecuency':np.nan
+  })
 
+# Frecuencias absolutas, frecuencias relativas y frecuencias acumuladas
+#"This is a cat: \N{Cat}" From: https://symbl.cc/es/
 
+longitud = len(df_goodyear["Price"])
+x = 0
+f = []
+acum = 0
+while x < len(lim):
+    for price in df_goodyear["Price"]:
+      s = df_frecuency.at[x, 'Price']
+      if price in s:
+        f.append(price)
+    # valores de Frecuencias absolutas
+    df_frecuency.at[x, 'frecuency']=len(f)
+    # Valores de Frecuencias relativas
+    df_frecuency.at[x, 'relative frecuency']=len(f)/longitud
+    # Valores de Frecuencias acumulativas
+    df_frecuency.at[x, 'cumulative frecuency']=len(f)+acum
+    acum = df_frecuency.at[x, 'cumulative frecuency']
+    x += 1    
+    f.clear()
+    
+# Formatear tabla #
 
+df_frecuency.style\
+  .format(precision=3, thousands=",", decimal=".")\
+  .format_index(str.upper, axis=1)
+
+##### HISTOGRAMA PANDAS #####
+
+df_frecuency.plot.bar(x='Price',
+                      y='frecuency',
+                      rot=0,
+                      width=0.9,
+                      figsize=(8,6)
+                      ).set_title('Histogram \nPrice')
+
+histograma_pd = df_goodyear["Price"].hist(alpha=0.8, bins=7)
+
+##### HISTOGRAMA PLOTLY#####
 
 histograma_px = px.histogram(df_goodyear["Price"], x="Price", text_auto=True, nbins=5)
 histograma_px.update_layout(bargap=0.01)
 histograma_px.show()
-
-histograma_pd = df_goodyear["Price"].hist(alpha=0.8, bins=5)
