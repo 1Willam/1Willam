1210d7d03bbd76fe43ef0e1cc33a39ce02ac1754
1Willam <72182022+1Willam@users.noreply.github.com>
2023-06-10 23:00:52 -0500

    Update function_tab_freq.py

--- function_tab_freq.py
+++ function_tab_freq.py
@@ -19,15 +19,22 @@ df_goodyear = pd.DataFrame(goodyear)
 
 #%%
 
-def tab_freq(dataframe, column=1):
+def tab_freq(dataframe, column=0, k=0, i=0):
   # Variables
   b = dataframe.axes[1][column]
   label = dataframe[f"{b}"]
-  k = int(np.round(np.emath.logn(2, len(label)), 0))
-  i = int(np.ceil((max(label)-min(label))/k))
-  #i = int(np.ceil(((max(label)-min(label))/k)/5)*5)
-  lim_s = pd.interval_range(start=int(np.floor(min(label))), periods=k-1, freq=i, closed='left')
-  lim_i = pd.interval_range(start=(k-1)*i+int(np.floor(min(label))), periods=1, freq=i, closed='both')
+  if k==0:
+    K = int(np.round(np.emath.logn(2, len(label)), 0))
+  else:
+    K=k
+  if i==0:
+    I = int(np.ceil(((max(label)-min(label))/K)/5)*5)
+  else:
+    I=i
+  #I = int(np.ceil((max(label)-min(label))/K))
+  #I = int(np.ceil(((max(label)-min(label))/K)/5)*5)
+  lim_s = pd.interval_range(start=int(np.floor(min(label))), periods=K-1, freq=I, closed='left')
+  lim_i = pd.interval_range(start=(K-1)*I+int(np.floor(min(label))), periods=1, freq=I, closed='both')
   lim = lim_s.append(lim_i)
   longitud = len(label)
   x = 0
@@ -63,4 +70,4 @@ def tab_freq(dataframe, column=1):
                         figsize=(8,6)
                         ).set_title(f'Histogram \n{b}')
   # Mostrar la tabla de fecuencias
-  print(f"k={k}, i={i}, lim={lim}, min={min(label)}, max={max(label)}\n", df_frequency)
+  print(f"K={K}, \nI={I}, \nlim={lim}, \nmin={min(label)}, max={max(label)}\n", df_frequency)
